ea6391742d2470858f515803b7fa5d90ac13ae1f
Jack B. Du <jd2804@nyu.edu>
2020-07-26 14:57:59 +0400

    external css and script

---
+++ js/script.js
@@ -0,0 +1,197 @@
+var host = findGetParameter("host");
+var debug = findGetParameter("debug");
+
+document.getElementById("zl").addEventListener('touchStart', function(event) {
+  holdButton(event);
+});
+document.getElementById("zl").addEventListener('click', function(event) {
+  clickButton(event);
+});
+document.getElementById("zr").addEventListener('click', function(event) {
+  clickButton(event);
+});
+document.getElementById("l").addEventListener('click', function(event) {
+  clickButton(event);
+});
+document.getElementById("r").addEventListener('click', function(event) {
+  clickButton(event);
+});
+document.getElementById("l_stick").addEventListener('click', function(event) {
+  clickButton(event);
+});
+document.getElementById("r_stick").addEventListener('click', function(event) {
+  clickButton(event);
+});
+document.getElementById("minus").addEventListener('click', function(event) {
+  clickButton(event);
+});
+document.getElementById("plus").addEventListener('click', function(event) {
+  clickButton(event);
+});
+document.getElementById("capture").addEventListener('click', function(event) {
+  clickButton(event);
+});
+document.getElementById("home").addEventListener('click', function(event) {
+  clickButton(event);
+});
+document.getElementById("x").addEventListener('click', function(event) {
+  clickButton(event);
+});
+document.getElementById("a").addEventListener('click', function(event) {
+  clickButton(event);
+});
+document.getElementById("b").addEventListener('click', function(event) {
+  clickButton(event);
+});
+document.getElementById("y").addEventListener('click', function(event) {
+  clickButton(event);
+});
+document.getElementById("up").addEventListener('click', function(event) {
+  clickButton(event);
+});
+document.getElementById("right").addEventListener('click', function(event) {
+  clickButton(event);
+});
+document.getElementById("down").addEventListener('click', function(event) {
+  clickButton(event);
+});
+document.getElementById("left").addEventListener('click', function(event) {
+  clickButton(event);
+});
+
+function display(str) {
+  if (debug === 'true') {
+    var display = document.getElementById("display");
+    display.style.display = 'block';
+    var escaped = str.replace(/&/, "&amp;").replace(/</, "&lt;").
+        replace(/>/, "&gt;").replace(/"/, "&quot;"); // "
+    display.innerHTML = "$ "+escaped;
+    console.log("DEBUG: "+escaped);
+  }
+}
+
+// get parameter by name
+// https://stackoverflow.com/questions/5448545/how-to-retrieve-get-parameters-from-javascript
+function findGetParameter(parameterName) {
+    var result = null,
+        tmp = [];
+    var items = location.search.substr(1).split("&");
+    for (var index = 0; index < items.length; index++) {
+        tmp = items[index].split("=");
+        if (tmp[0] === parameterName) result = decodeURIComponent(tmp[1]);
+    }
+    return result;
+}
+
+var ws;
+
+function init() {
+
+  display("Welcome to Web-Con!");
+
+  // Connect to Web Socket
+  ws = new WebSocket(host);
+
+  // Set event handlers.
+  ws.onopen = function() {
+    console.log("onopen");
+  };
+
+  ws.onmessage = function(e) {
+    // e.data contains received string.
+    console.log("onmessage: " + e.data);
+  };
+
+  ws.onclose = function() {
+    console.log("onclose");
+  };
+
+  ws.onerror = function(e) {
+    console.log("onerror");
+    console.log(e)
+  };
+
+}
+
+function clickButton(event) {
+  var button = event.target.id || event.srcElement.id;
+  display("Button <" + button + "> pushed");
+  ws.send('c,'+button);
+}
+
+dragElement(document.getElementById("l_stick"));
+dragElement(document.getElementById("r_stick"));
+
+function dragElement(elem) {
+  var initX = 0, initY = 0, posX = 0, posY = 0;
+  var constraint = elem.clientWidth/2;
+
+  elem.ontouchstart = dragStart;
+  elem.onmousedown = dragStart;
+
+  function dragStart(e) {
+    e = e || window.event;
+    e.preventDefault();
+    // get the touch position at startup:
+    initX = e.pageX;
+    initY = e.pageY;
+    if(e.targetTouches) {
+      for (var i = 0; i < e.targetTouches.length; i++) {
+        if (e.targetTouches[i].target == elem) {
+          initX = e.targetTouches[i].clientX;
+          initY = e.targetTouches[i].clientY;
+          break;
+        }
+      }
+    }
+    elem.ontouchend = dragEnd;
+    elem.ontouchcancel = dragEnd;
+    elem.ontouchmove = dragMove;
+    document.onmouseup = dragEnd;
+    document.onmousemove = dragMove;
+    console.log('Drag started at '+initX+', '+initY);
+  }
+
+  function dragMove(e) {
+    e = e || window.event;
+    e.preventDefault();
+    // calculate the new touch position:
+    posX = e.pageX - initX;
+    posY = e.pageY - initY;
+    if(e.targetTouches) {
+      for (var i = 0; i < e.targetTouches.length; i++) {
+        if (e.targetTouches[i].target == elem) {
+          posX = e.targetTouches[i].clientX - initX;
+          posY = e.targetTouches[i].clientY - initY;
+          break;
+        }
+      }
+    }
+    tmag = Math.sqrt(posX**2+posY**2);
+    if (tmag > constraint) {
+      posY = posY*constraint/tmag;
+      posX = posX*constraint/tmag;
+    }
+    elem.style.top = posY + "px";
+    elem.style.left = posX + "px";
+    var hVal = Math.round(4095*(Math.sin(posX/constraint/2*Math.PI)+1)/2).toString();
+    var vVal = Math.round(4095*(Math.sin(-posY/constraint/2*Math.PI)+1)/2).toString();
+    ws.send('s,'+elem.id.charAt(0)+',h,'+hVal)
+    ws.send('s,'+elem.id.charAt(0)+',v,'+vVal)
+    console.log('Drag moved: s,'+elem.id.charAt(0)+',h,'+hVal);
+  }
+
+  function dragEnd() {
+    // stop moving when touch ends:
+    elem.ontouchend = null;
+    elem.ontouchcancel = null;
+    elem.ontouchmove = null;
+    document.onmouseup = null;
+    document.onmousemove = null;
+    elem.style.top = "0px";
+    elem.style.left = "0px";
+    console.log('Drag ended: s,'+elem.id.charAt(0)+',center,2048');
+    ws.send('s,'+elem.id.charAt(0)+',center,2048')
+  }
+
+}
